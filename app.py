59e3244eccd63a27f509d1c94d84b53b572016c4
yaslittlepanda <155705096+yaslittlepanda@users.noreply.github.com>
2024-01-17 10:33:13 -0800

    Confirmation(Update)

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
+@app.route('/')
+def hello_world():
     return render_template("index.html")
 
 @app.route('/about')
@@ -17,6 +17,17 @@ def products():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/confirmation')
+def confirmation():
+    name = request.args.get("name")
+    email = request.args.get("email")
+    print(name)
+    props = {
+        "name": name,
+        "email": email
+    }
+    return render_template("confirmation.html", data=props)
 
 if __name__ == '__main__':
     app.run(debug=True)
